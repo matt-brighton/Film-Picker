efcf19df74319dfaf28eab358f20ec71084c517b
matt-brighton <matthew.brighton@hmrc.gov.uk>
2025-07-17 12:51:00 +0100

    API Key added

--- Film_Picker.py
+++ Film_Picker.py
@@ -1,12 +1,14 @@
+from dotenv import load_dotenv
+import os
 import streamlit as st
 # https://jnbk3wnuzk2wpvtczpuwmc.streamlit.app
+
+load_dotenv()
+TMDB_API_KEY = os.getenv("TMDB_API_KEY")
 
 st.set_page_config(
     page_title="Film Picker"
 )
 
-
 st.title("Welcome to the Film Picker")
 st.write("Please use the sidebar to navigate!")
-
-
