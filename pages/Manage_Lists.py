411b6a0b32c415221a3653a7e3bc3b317f5977c1
matt-brighton <matthew.brighton@hmrc.gov.uk>
2025-07-19 18:08:00 +0100

    added lists

--- pages/Manage_Lists.py
+++ pages/Manage_Lists.py
@@ -1,11 +1,15 @@
 import streamlit as st
 import pandas as pd
 import json
-from utils import get_films
+from utils import get_films, get_film_directory
 
-films = get_films()
-
-df = pd.DataFrame(films)
+raw_files, formatted_files = get_film_directory()
+file_map = dict(zip(formatted_files, raw_files))
 
 st.title("Manage Lists")
-st.dataframe(df)
+selected_film_list = st.selectbox("Choose a List", (formatted_files))
+selected_file = file_map[selected_film_list]
+if st.button("Select List", type="primary"):
+    films = get_films(selected_file)
+    data = pd.DataFrame(films)
+    st.dataframe(data)
